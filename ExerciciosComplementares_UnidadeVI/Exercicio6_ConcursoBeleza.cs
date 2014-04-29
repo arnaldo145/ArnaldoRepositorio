5ed1d420b0e9c1b455849bbfe672e2eda6a09023
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-04-29 11:45:52 -0300

    Atualizando

---
+++ ExerciciosComplementares_UnidadeVI/Exercicio6_ConcursoBeleza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosComplementares_UnidadeVI
+{
+    class Exercicio6_ConcursoBeleza
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
