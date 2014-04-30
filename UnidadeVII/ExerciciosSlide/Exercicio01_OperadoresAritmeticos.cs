4f208cc8875396bc205376846fdd1af5b4a06599
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-04-30 11:44:57 -0300

    Atualizado

--- UnidadeVII/ExerciciosSlide/Exercicio01_OperadoresAritmeticos.cs
+++ UnidadeVII/ExerciciosSlide/Exercicio01_OperadoresAritmeticos.cs
@@ -10,7 +10,14 @@ namespace UnidadeVII.ExerciciosSlide
     {
         static void Main(string[] args)
         {
-
+            int soma = 3 + 2;
+            int subtracao = 10 - 5;
+            int multiplicacao = 2 * 5;
+            int divisao = 10 / 2;
+            int modulo = 15 % 5;
+            double divCast = (double)5 / 2;
+            Console.WriteLine("Soma: {0} \nSubtração: {1} \nMultiplicação: {2} \nDivisão: {3} \nMódulo: {4}", soma, subtracao, multiplicacao, divisao, modulo);
+            Console.ReadKey();
         }
     }
 }
