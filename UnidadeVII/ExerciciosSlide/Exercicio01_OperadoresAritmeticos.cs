ce7ce01e359cdd07287e090efab3faeca37c2c8a
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-04-30 11:39:25 -0300

    Unidade 7

---
+++ UnidadeVII/ExerciciosSlide/Exercicio01_OperadoresAritmeticos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnidadeVII.ExerciciosSlide
+{
+    class Exercicio01_OperadoresAritmeticos
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
