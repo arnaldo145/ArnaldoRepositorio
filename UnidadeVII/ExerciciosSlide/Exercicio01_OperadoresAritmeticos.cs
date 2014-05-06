7a890eca0d887c8252dbc40bdce86349326bd962
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-05-06 08:35:47 -0300

    Exercicios do Slide Atualizados

--- UnidadeVII/ExerciciosSlide/Exercicio01_OperadoresAritmeticos.cs
+++ UnidadeVII/ExerciciosSlide/Exercicio01_OperadoresAritmeticos.cs
@@ -11,23 +11,40 @@ namespace UnidadeVII.ExerciciosSlide
         static void Main(string[] args)
         {
             //Na classe com nome Operadores, implemente um programa em C# que utilize todos os operadores aritméticos.
+
             int resultado = 0;
+
             resultado = 1 + 1;
+
             resultado = 2 - 1;
+
             resultado = 3 * 2;
+
             resultado = 10 / 2;
+
             resultado = 10 % 5;
 
+
+
             //Na pasta operadores, implemente um programa em C# que realize operações de divisão inteira e de casting.
+
             resultado = 5 / 2;
+
             Console.WriteLine((double)7 / 2);
 
+
             //Na pasta operadores, implemente um programa em C# que realize operações de concatenação de strings.
+
             string nome = "Arnaldo";
+
             string space = " ";
+
             string sobrenome = "Madeira";
+
             string result = nome + space + sobrenome;
+
             Console.WriteLine(result);
+
 
             Console.ReadKey();
         }
