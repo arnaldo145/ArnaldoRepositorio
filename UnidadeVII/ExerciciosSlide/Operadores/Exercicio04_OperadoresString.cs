c3e22ba6fb400163c812c2584631029569b300b8
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-05-08 09:18:26 -0300

    exemplos operadores string

--- UnidadeVII/ExerciciosSlide/Operadores/Exercicio04_OperadoresString.cs
+++ UnidadeVII/ExerciciosSlide/Operadores/Exercicio04_OperadoresString.cs
@@ -10,7 +10,34 @@ namespace UnidadeVII.ExerciciosSlide.Operadores
     {
         static void Main(string[] args)
         {
+            //Contar carcteres de um string
+            string n = "Arnaldo";
+            int qtd = n.Length;
+            Console.WriteLine("{0} tem {1} caracteres", n, qtd);
 
+            //Pegar um caractere de um string
+            char c = n[0];
+            Console.WriteLine("{0}", c);
+
+
+            string s = "Apostila de Java";
+
+            bool resultado1 = s.Contains("C#");
+            bool resultado2 = s.Contains("Java");
+            Console.WriteLine(resultado1);
+            Console.WriteLine(resultado2);
+
+            resultado1 = s.EndsWith("C#");
+            resultado2 = s.EndsWith("Java");
+            Console.WriteLine(resultado1);
+            Console.WriteLine(resultado2);
+
+            resultado1 = s.StartsWith("Apostila");
+            resultado2 = s.StartsWith("Java");
+            Console.WriteLine(resultado1);
+            Console.WriteLine(resultado2);
+
+            Console.ReadKey();
         }
     }
 }
