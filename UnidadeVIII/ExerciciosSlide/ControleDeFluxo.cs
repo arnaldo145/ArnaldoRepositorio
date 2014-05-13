c7f8c1fdc94238e5f6631a1d84d38e71146401cc
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-05-13 10:05:05 -0300

    Atualizando Controle de Fluxo

--- UnidadeVIII/ExerciciosSlide/ControleDeFluxo.cs
+++ UnidadeVIII/ExerciciosSlide/ControleDeFluxo.cs
@@ -10,17 +10,20 @@ namespace UnidadeVIII.ExerciciosSlide
     {
         public static void Main()
         {
-            //Aprovado-Reprovado
             Random gerador = new Random();
+
+            //Aprovado-Reprovado   
             double nota = gerador.NextDouble();
-            if (nota > 0.7)
-                Console.WriteLine("Aluno foi aprovado com a nota {0:F1}", nota);
-            else
-                Console.WriteLine("Aluno foi reprovado com a nota {0:F1}", nota);
+            Console.WriteLine("{0} com a nota {1:F1}",(nota > 0.7 ? "Aluno foi aprovado" : "Aluno foi reprovado"), nota);
 
             //Verifica Valor do Produto
             double valor = gerador.NextDouble();
-            Console.WriteLine("R${0:F2}: {1}", valor, (valor > 0.5) ? "Preço é caro" : "Preço está em conta");
+            Console.WriteLine("R${0:F2}: {1}", valor, (valor > 0.5) ? "Preço é caro" : "Preço está em conta");      
+
+            //EscolheCaminho
+            double caminho = gerador.NextDouble();
+            Console.WriteLine(caminho > 0.5 ? "Siga para direita" : "Siga para esquerda");
+
             Console.ReadKey();
         }
     }
