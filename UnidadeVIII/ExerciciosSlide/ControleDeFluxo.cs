e2a21600de37ca30f9ab02c30723ef83f274a370
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-05-13 09:46:55 -0300

    Atualizando Controle de FLuxo

--- UnidadeVIII/ExerciciosSlide/ControleDeFluxo.cs
+++ UnidadeVIII/ExerciciosSlide/ControleDeFluxo.cs
@@ -10,12 +10,17 @@ namespace UnidadeVIII.ExerciciosSlide
     {
         public static void Main()
         {
+            //Aprovado-Reprovado
             Random gerador = new Random();
             double nota = gerador.NextDouble();
             if (nota > 0.7)
                 Console.WriteLine("Aluno foi aprovado com a nota {0:F1}", nota);
             else
                 Console.WriteLine("Aluno foi reprovado com a nota {0:F1}", nota);
+
+            //Verifica Valor do Produto
+            double valor = gerador.NextDouble();
+            Console.WriteLine("R${0:F2}: {1}", valor, (valor > 0.5) ? "Preço é caro" : "Preço está em conta");
             Console.ReadKey();
         }
     }
