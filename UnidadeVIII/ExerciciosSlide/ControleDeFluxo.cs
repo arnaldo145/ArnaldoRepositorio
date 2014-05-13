9604412e576cf53ba64a9afa66e655530a59e013
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-05-13 10:38:13 -0300

    Atualizando Exercicios Controle de Fluxo

--- UnidadeVIII/ExerciciosSlide/ControleDeFluxo.cs
+++ UnidadeVIII/ExerciciosSlide/ControleDeFluxo.cs
@@ -14,15 +14,51 @@ namespace UnidadeVIII.ExerciciosSlide
 
             //Aprovado-Reprovado   
             double nota = gerador.NextDouble();
-            Console.WriteLine("{0} com a nota {1:F1}",(nota > 0.7 ? "Aluno foi aprovado" : "Aluno foi reprovado"), nota);
+            if (nota > 0.7)
+            {
+                Console.WriteLine("Aluno foi aprovado");
+            } else{
+                Console.WriteLine("Aluno foi reprovado");
+            }
 
             //Verifica Valor do Produto
-            double valor = gerador.NextDouble();
-            Console.WriteLine("R${0:F2}: {1}", valor, (valor > 0.5) ? "Preço é caro" : "Preço está em conta");      
+            double valor = gerador.NextDouble(); 
+            if(valor > 0.5){
+                Console.WriteLine("Preço é caro");
+            } else {
+                Console.WriteLine("Preço é caro");
+            }
 
             //EscolheCaminho
             double caminho = gerador.NextDouble();
-            Console.WriteLine(caminho > 0.5 ? "Siga para direita" : "Siga para esquerda");
+            if (caminho > 0.5)
+            {
+                Console.WriteLine("Siga para direita");
+            } else {
+                Console.WriteLine("Siga para esquerda");
+            }
+
+            //EscolheRoupa
+            double roupa = gerador.NextDouble();
+            if (caminho > 0.5)
+            {
+                Console.WriteLine("Vista uma camisa vermelha");
+            } else {
+                Console.WriteLine("Vista uma camisa azul");
+            }
+
+            //ADividioPorB
+            int a = gerador.Next(1,100);
+            int b = gerador.Next(1,100);
+
+            if (a % b == 0)
+            {
+                Console.WriteLine("{0} é divisivel por {1}", a, b);
+            }
+            else
+            {
+                Console.WriteLine("{0} não é divisivel por {1}", a, b);
+            }
 
             Console.ReadKey();
         }
