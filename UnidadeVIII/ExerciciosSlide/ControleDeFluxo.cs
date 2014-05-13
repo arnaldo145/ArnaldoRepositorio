9571bc5fd6eb496280d42ce10fc1a74a60cd22af
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-05-13 10:53:28 -0300

    Atualizando

--- UnidadeVIII/ExerciciosSlide/ControleDeFluxo.cs
+++ UnidadeVIII/ExerciciosSlide/ControleDeFluxo.cs
@@ -15,8 +15,7 @@ namespace UnidadeVIII.ExerciciosSlide
             //Aprovado-Reprovado   
             double nota = gerador.NextDouble();
             Console.WriteLine("Nota: {0:F1}", nota);
-            if (nota > 0.7)
-            {
+            if (nota > 0.7) {
                 Console.WriteLine("Aluno foi aprovado");
             } else{
                 Console.WriteLine("Aluno foi reprovado");
@@ -25,7 +24,7 @@ namespace UnidadeVIII.ExerciciosSlide
             //Verifica Valor do Produto
             double valor = gerador.NextDouble();
             Console.WriteLine("\nPreço: R${0:F2}",valor);
-            if(valor > 0.5){
+            if(valor > 0.5) {
                 Console.WriteLine("Preço é caro");
             } else {
                 Console.WriteLine("Preço é caro");
@@ -34,8 +33,7 @@ namespace UnidadeVIII.ExerciciosSlide
             //EscolheCaminho
             double caminho = gerador.NextDouble();
             Console.WriteLine("\nCaminho: {0:F2}", caminho);
-            if (caminho > 0.5)
-            {
+            if (caminho > 0.5) {
                 Console.WriteLine("Siga para direita");
             } else {
                 Console.WriteLine("Siga para esquerda");
@@ -44,8 +42,7 @@ namespace UnidadeVIII.ExerciciosSlide
             //EscolheRoupa
             double roupa = gerador.NextDouble();
             Console.WriteLine("\nRoupa: {0:F2}", roupa);
-            if (caminho > 0.5)
-            {
+            if (caminho > 0.5) {
                 Console.WriteLine("Vista uma camisa vermelha");
             } else {
                 Console.WriteLine("Vista uma camisa azul");
@@ -55,8 +52,7 @@ namespace UnidadeVIII.ExerciciosSlide
             int a = gerador.Next(1,100);
             int b = gerador.Next(1,100);
 
-            if (a % b == 0)
-            {
+            if (a % b == 0) {
                 Console.WriteLine("\n{0} é divisivel por {1}", a, b);
             } else {
                 Console.WriteLine("\n{0} não é divisivel por {1}", a, b);
