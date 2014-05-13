3e6fc1f5623e7c56db03a9b6de9d5b021e8a3844
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-05-13 09:39:52 -0300

    Controle de FLuxo - Exercicio

---
+++ UnidadeVIII/ExerciciosSlide/ControleDeFluxo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnidadeVIII.ExerciciosSlide
+{
+    class ControleDeFluxo
+    {
+        public static void Main()
+        {
+            Random gerador = new Random();
+            double nota = gerador.NextDouble();
+            if (nota > 0.7)
+                Console.WriteLine("Aluno foi aprovado com a nota {0:F1}", nota);
+            else
+                Console.WriteLine("Aluno foi reprovado com a nota {0:F1}", nota);
+            Console.ReadKey();
+        }
+    }
+}
