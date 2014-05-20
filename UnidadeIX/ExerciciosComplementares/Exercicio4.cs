be9e6c7ae7a4c943a0cf386de4e8967373e6f49f
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-05-20 09:40:42 -0300

    Exercicios Complementares - uNidade 9

--- UnidadeIX/ExerciciosComplementares/Exercicio4.cs
+++ UnidadeIX/ExerciciosComplementares/Exercicio4.cs
@@ -10,9 +10,38 @@ namespace UnidadeIX.ExerciciosComplementares
     {
         //Escreva um algoritmo que leia um vetor inteiro de 12 posições e crie um segundo vetor, substituindo os valores nulos por 1. Mostre os 2 vetores.
 
-        static void Main(string[] args)
+        static void Main4(string[] args)
         {
+            int[] vetor1 = new int[12];
+            int[] vetor2 = new int[12];
+            Random gerador = new Random();
 
+            Console.WriteLine(" ===== COMPARAÇÃO DE VETORES =====");
+
+            for (int i = 0; i < vetor1.Length; i++)
+            {
+                int val = gerador.Next(0, 10);
+                vetor1[i] = val;
+
+                if (val == 0)
+                    vetor2[i] = 1;
+                else
+                    vetor2[i] = val;
+            }
+
+            Console.Write("\nVetor 1: ");
+            for (int i = 0; i < vetor1.Length; i++)
+            {
+                Console.Write("{0} ", vetor1[i]);
+            }
+
+            Console.Write("\nVetor 2: ");
+            for (int i = 0; i < vetor2.Length; i++)
+            {
+                Console.Write("{0} ", vetor2[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ UnidadeIX/ExerciciosComplementares/Exercicio5.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnidadeIX.ExerciciosComplementares
+{
+    class Exercicio5
+    {
+        //Escreva um algoritmo que leia um vetor de 10 posições do tipo caracter, que representa o gabarito de uma prova. A seguir, 
+        //para cada um dos 20 alunos da turma, leia o vetor de respostas (R) do aluno de 10 posições do tipo caracter e conte o 
+        //número de acertos. Mostre o nº de acertos do aluno e uma mensagem APROVADO, se a nota for maior ou igual a 6; e mostre uma 
+        //mensagem de REPROVADO, caso contrário.
+
+        static void Main(string[] args)
+        {
+            Random gerador = new Random();
+            char[] gabarito = new char[10];
+            char[] questoes = new char[]{'A', 'B', 'C', 'D'};
+
+            Console.WriteLine(" ===== Gabarito ===== \n");
+            for (int i = 0; i < gabarito.Length; i++)
+            {
+                gabarito[i] = questoes[gerador.Next(0, questoes.Length)];
+                Console.WriteLine("Questão {0}: {1}", (i+1), gabarito[i]);
+            }
+
+            Console.WriteLine(" ");
+
+            for (int i = 0; i < 20; i++)
+            {
+                int count = 0;
+                Console.WriteLine(" ----- Aluno {0} -----", (i + 1));
+
+                char[] resposta = new char[10];
+
+                for (int j = 0; j < resposta.Length; j++)
+                {
+                    resposta[j] = questoes[gerador.Next(0, questoes.Length)];
+                    if(resposta[j] == gabarito[j]){
+                        count++;
+                    }
+                }
+
+                Console.WriteLine("\nNúmero de acertos: {0}", count);
+                Console.WriteLine("{0} \n", count >= 6 ? "Aprovado" : "Reprovado");
+            }
+                Console.ReadKey();
+        }
+    }
+}
