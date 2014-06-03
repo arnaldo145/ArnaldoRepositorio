37ded67c036d0677305614a27a1bfbd327ee5f5f
arnaldo145 <arnaldomadeira11@hotmail.com>
2014-06-03 10:16:02 -0300

    Atualizando

--- UnidadeXI/ExerciciosFixacao/OrdenaElementos.cs
+++ UnidadeXI/ExerciciosFixacao/OrdenaElementos.cs
@@ -14,20 +14,18 @@ namespace UnidadeXI.ExerciciosFixacao
         {
             int[] valores = new int[50];
             Random geraNum = new Random();
-
             Console.WriteLine(" ===== DESORDENADO =====");
             for (int i = 0; i < valores.Length; i++)
             {
                 valores[i] = geraNum.Next(0, 30);
-                Console.Write("{0} ", valores[i]);
+                Console.Write("{0} ", valores[i]);                
             }
-
+            Console.WriteLine(" ");
             Ordenar(valores);
-
             Console.WriteLine("\n ===== ORDENADO =====");
             for (int i = 0; i < valores.Length; i++)            
                 Console.Write("{0} ", valores[i]);
-
+            Console.WriteLine(" ");
             Console.WriteLine("\nENTER para sair: ");
             Console.ReadKey();
         }
@@ -42,24 +40,18 @@ namespace UnidadeXI.ExerciciosFixacao
         public static int Menor(int[] array, int inicio)
         {
             int menor = inicio;
-
             for (int i = inicio + 1; i < array.Length; i++)
             {
-                if (array[menor] > array[i])
-                {
-                    menor = i;
-                }
+                if (array[menor] > array[i])                
+                    menor = i;                
             }
             return menor;
         }
 
         public static void Ordenar(int[] array)
         {
-            for (int i = 0; i < array.Length; i++)
-            {
-                int menor = Menor(array, i);
-                Troca(array, i, menor);
-            }
+            for (int i = 0; i < array.Length; i++)            
+                Troca(array, i, Menor(array, i));            
         }
     }
 }
